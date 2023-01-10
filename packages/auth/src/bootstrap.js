53cba3a963fc1bf034843cf8273a859881ab4da4
Dhanush S <dhas@WKMZT7418515.global.publicisgroupe.net>
2023-01-10 22:27:08 +0530

    Handling CSS in Microfrontend

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -1,3 +1,4 @@
+import './MuiClassNameSetup';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createMemoryHistory, createBrowserHistory } from 'history';
--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,7 +1,7 @@
 import React, { lazy, Suspense, useState, useEffect } from "react";
 import { Router, Route, Switch } from "react-router-dom";
 import { createBrowserHistory } from "history";
-
+// import { StylesProvider, createGenerateClassName } from "@mui/styles";
 import Progress from "./components/Progress";
 import Header from "./components/Header";
 import ErrorBoundary from "./components/ErrorBoundary";
@@ -9,14 +9,15 @@ import NotFound from "./components/NotFound";
 import { PrivateRoute } from "./routes/privateRoute";
 import { AdminRoute } from "./routes/adminRoute";
 
-
 const MarketingPage = lazy(() => import("./components/apps/MarketingApp"));
 const AuthPage = lazy(() => import("./components/apps/AuthApp"));
 const DashboardPage = lazy(() => import("./components/apps/DashboardApp"));
 const TablePage = lazy(() => import("./components/apps/DodgeTableApp"));
 
-
 const history = createBrowserHistory();
+// const generateClassName = createGenerateClassName({
+//   productionPrefix: "container",
+// });
 
 export default () => {
   const [isSignedIn, setIsSignedIn] = useState(false);
@@ -35,8 +36,8 @@ export default () => {
   }, [isSignedIn]);
 
   return (
-    <ErrorBoundary>
-      <Router history={history}>
+      <ErrorBoundary>
+        <Router history={history}>
           <div>
             <Header
               onSignOut={() => setIsSignedIn(false)}
@@ -47,7 +48,11 @@ export default () => {
                 <Route path="/auth">
                   <AuthPage onSignIn={() => setIsSignedIn(true)} />
                 </Route>
-                <AdminRoute path="/dashboard" component={DashboardPage} role={1001} />
+                <AdminRoute
+                  path="/dashboard"
+                  component={DashboardPage}
+                  role={1001}
+                />
                 <PrivateRoute
                   path="/dodge"
                   component={TablePage}
@@ -58,8 +63,8 @@ export default () => {
               </Switch>
             </Suspense>
           </div>
-      </Router>
-    </ErrorBoundary>
+        </Router>
+      </ErrorBoundary>
   );
 };
 
