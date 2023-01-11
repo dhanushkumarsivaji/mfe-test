2108fd8f44493f862cd0e7f92cc7540070a029dd
Dhanush S <dhas@WKMZT7418515.global.publicisgroupe.net>
2023-01-11 23:40:26 +0530

    Added build and deploy worflows

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -75,3 +75,17 @@ export default () => {
                   <DashboardPage />
   </Route> */
 }
+
+
+// {
+//   "clientId": "feacbad9-0350-4b82-8642-a1a9e6abccaa",
+//   "clientSecret": "lF48Q~Wdg2eBEadl1o2h-2XU8PiQ3N9Yu_Ihddre",
+//   "subscriptionId": "351b6525-81b1-48bf-a6e8-4bd5da72e974",
+//   "tenantId": "39de4c31-5af6-43b4-8418-bb6a45179340",
+//   "activeDirectoryEndpointUrl": "https://login.microsoftonline.com",
+//   "resourceManagerEndpointUrl": "https://management.azure.com/",
+//   "activeDirectoryGraphResourceId": "https://graph.windows.net/",
+//   "sqlManagementEndpointUrl": "https://management.core.windows.net:8443/",
+//   "galleryEndpointUrl": "https://gallery.azure.com/",
+//   "managementEndpointUrl": "https://management.core.windows.net/"
+// }
